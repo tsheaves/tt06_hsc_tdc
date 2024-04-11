df0f29bb460e3d03534869ca8809073f9204411e
Tyler Sheaves <tsheaves@ucdavis.edu>
2024-04-11 14:25:34 -0700

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -16,7 +16,7 @@ set ::env(PL_TARGET_DENSITY) 0.8
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 15.1515ns ~= 66MHz.
-set ::env(CLOCK_PERIOD) "54"
+set ::env(CLOCK_PERIOD) "55"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
 set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.1
