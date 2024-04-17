068592fce49223c1a13a84790767bfb1c60b3749
Tyler Sheaves <sheaves94@gmail.com>
2024-04-17 10:45:43 -0700

    documentation added vcd2json

--- test/tdc_vcd_to_json.py
+++ test/tdc_vcd_to_json.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from vcd2json.vcd2json import WaveExtractor
 
-def process_vcd(path_list, vcd, json):
-    extractor = WaveExtractor('test.vcd', 'timer.json', path_list)
-    extractor.execute()
+# def process_vcd(path_list, vcd, json):
+#     extractor = WaveExtractor('test.vcd', 'timer.json', path_list)
+#     extractor.execute()
 
-if __name__ == '__main__':
-    path_list = [
-        "tb.clk_launch",
-        "tb.clk_capt"   
-    ]
-    process_vcd(path_list, "tb.vcd", "tb_wf.json")
+# if __name__ == '__main__':
+#     path_list = [
+#         "tb.clk_launch",
+#         "tb.clk_capt"   
+#     ]
+#     process_vcd(path_list, "tb.vcd", "tb_wf.json")
 
